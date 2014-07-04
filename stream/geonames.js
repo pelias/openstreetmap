63b0915aa3c4cbf606bc456f1ac68a3e893b45a9
Peter Johnson <wiz@wiz.co.nz>
2014-07-04 18:46:15 +0100

    ~2x import speed improvements, experimenting with geohash_cell filtering

--- stream/geonames.js
+++ stream/geonames.js
@@ -4,21 +4,43 @@ var esclient = require('pelias-esclient')();
 
 var stream = through.obj( function( item, enc, done ) {
 
+  // skip lookup if we already have geo info
+  if( item.admin0 && item.admin1 && item.admin2 ) return done();
+
   esclient.search({
     index: 'pelias',
     type: 'geoname',
     body: {
+      "fields": [ "admin0", "admin1", "admin2" ],
       "query": {
         "filtered": {
           "query": {
             "match_all": {}
           },
           "filter" : {
-            "geo_distance" : {
-              "distance": "200km",
-              "distance_type": "plane",
-              "optimize_bbox": "indexed",
-              "center_point": item.center_point
+            "bool": {
+              "must": [
+                // {
+                //   "geohash_cell": {
+                //     "center_point": item.center_point,
+                //     "precision": 12, // @note: setting this higher than 5 causes loads of misses!?
+                //                      // ...and setting it around 5 is very very slow.
+                //     "neighbors": true
+                //   }
+                // },
+                {
+                  "geo_distance" : {
+                    "distance": "50km",
+                    "distance_type": "plane",
+                    "optimize_bbox": "indexed",
+                    "center_point": {
+                      "lat": Number( item.center_point.lat ).toFixed(2), // @note: make filter cachable
+                      "lon": Number( item.center_point.lon ).toFixed(2)  // precision max ~1.113km off
+                    },
+                    "_cache": true
+                  }
+                }
+              ]
             }
           }
         }
@@ -36,13 +58,15 @@ var stream = through.obj( function( item, enc, done ) {
 
     if( error ) console.error( error );
 
+    // console.log( JSON.stringify( resp, null, 2 ) );
+
     if( !error && 'object' == typeof resp && resp.hasOwnProperty('hits') && 
         Array.isArray( resp.hits.hits ) && resp.hits.hits.length ){
 
-      var hit = resp.hits.hits[0]._source;
-      item.admin0 = hit.admin0;
-      item.admin1 = hit.admin1;
-      item.admin2 = hit.admin2;
+      var hit = resp.hits.hits[0].fields;
+      if( hit.admin0 ) item.admin0 = hit.admin0[0];
+      if( hit.admin1 ) item.admin1 = hit.admin1[0];
+      if( hit.admin2 ) item.admin2 = hit.admin2[0];
     }
    
     this.push( item, enc );
