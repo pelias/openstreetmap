91480765c9a4e1f46904e1997feecc77c8f38f62
Peter Johnson <insomnia@rcpt.at>
2014-09-02 19:14:35 +0100

    deprecated old code

--- stream/osm2.js
+++ stream/osm2.js
@@ -1,45 +1,47 @@
 
-var osmread = require('../static_modules/osm-read');
-var PassThrough = require('stream').PassThrough;
+// deprecated
 
-module.exports = function( filepath ){
+// var osmread = require('../static_modules/osm-read');
+// var PassThrough = require('stream').PassThrough;
 
-  var stream = new PassThrough({ objectMode: true });
-  console.log( 'parse', filepath );
+// module.exports = function( filepath ){
 
-  osmread.parse({
-    filePath: filepath,
-    endDocument: function(){
-      console.log( 'endDocument' );
-      stream.close();
-    },
-    bounds: function( item, next ){
-      // process.stdout.write('B');
-      item.type = 'bounds';
-      stream.write( item, 'utf8', next );
-    },
-    node: function( item, next ){
-      // process.stdout.write('N');
-      item.type = 'node';
-      stream.write( item, 'utf8', next );
-    },
-    way: function( item, next ){
-      // process.stdout.write('W');
-      item.type = 'way';
-      stream.write( item, 'utf8', next );
-    },
-    relation: function( item, next ){
-      // process.stdout.write('R');
-      item.type = 'relation';
-      stream.write( item, 'utf8', next );
-    },
-    error: function( msg ){
-      console.error( msg );
-    }
-  });
+//   var stream = new PassThrough({ objectMode: true });
+//   console.log( 'parse', filepath );
+
+//   osmread.parse({
+//     filePath: filepath,
+//     endDocument: function(){
+//       console.log( 'endDocument' );
+//       stream.close();
+//     },
+//     bounds: function( item, next ){
+//       // process.stdout.write('B');
+//       item.type = 'bounds';
+//       stream.write( item, 'utf8', next );
+//     },
+//     node: function( item, next ){
+//       // process.stdout.write('N');
+//       item.type = 'node';
+//       stream.write( item, 'utf8', next );
+//     },
+//     way: function( item, next ){
+//       // process.stdout.write('W');
+//       item.type = 'way';
+//       stream.write( item, 'utf8', next );
+//     },
+//     relation: function( item, next ){
+//       // process.stdout.write('R');
+//       item.type = 'relation';
+//       stream.write( item, 'utf8', next );
+//     },
+//     error: function( msg ){
+//       console.error( msg );
+//     }
+//   });
   
-  // catch stream errors
-  stream.on( 'error', console.error.bind( console, __filename ) );
+//   // catch stream errors
+//   stream.on( 'error', console.error.bind( console, __filename ) );
 
-  return stream;
-}
+//   return stream;
+// }
