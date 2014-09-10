571b6d2009b37bd2e01f3f0f78b29a2f6dde6bef
Peter Johnson <insomnia@rcpt.at>
2014-09-10 18:44:02 +0100

    all streams exit cleanly; tests

--- stream/osm_types.js
+++ stream/osm_types.js
@@ -1,40 +1,58 @@
 
 var through = require('through2');
 
+function filterTypeStream( type ){
+  var stream = through.obj( function( data, enc, next ) {
+
+    // data is an array of items
+    if( Array.isArray( data ) ){
+      data.forEach( function( item ){
+        if( item.type === type ){
+          this.push( item );
+        }
+      }, this);
+    }
+
+    // data is a single item
+    else {
+      if( data.type === type ){
+        this.push( data );
+      }
+    }
+
+    next();
+
+  });
+  return stream;
+}
+
 module.exports = function( streams ){
   if( !streams || !streams.node || !streams.way || !streams.relation ){
     throw new Error( 'invalid types stream constructor' );
   }
 
-  var stream = through.obj( function( data, enc, done ) {
+  var node_stream = filterTypeStream( 'node' );
+  node_stream.pipe( streams.node );
 
-    // forward the data to the appropriate stream
-    var forward = function( item, enc, next ){
-      if( ['node','way','relation'].indexOf( item.type ) === -1 ){
-        console.error( 'unknown type', item.type );
-        return next();
-      }
-      return streams[ item.type ].write( item, enc, setImmediate.bind( this, next ) );
-    }
-    
-    // data is an array of items
-    if( Array.isArray( data ) ){
-      var shift = function(){
-        if( !data.length ) return done();
-        forward( data.shift(), enc, shift );
-      }
-      shift();
-    }
+  var way_stream = filterTypeStream( 'way' );
+  way_stream.pipe( streams.way );
 
-    // data is a single item
-    else {
-      forward( data, enc, done );
-    }
+  var relation_stream = filterTypeStream( 'relation' );
+  relation_stream.pipe( streams.relation );
 
+  // simple passthrough stream
+  var stream = through.obj( function( data, enc, next ) {
+    this.push( data );
+    next();
   });
+
+  // pipe to each filter stream
+  stream.pipe( node_stream );
+  stream.pipe( way_stream );
+  stream.pipe( relation_stream );
   
   // catch stream errors
   stream.on( 'error', console.error.bind( console, __filename ) );
 
   return stream;
-}
+};
