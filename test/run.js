980b54bc497e7f67371822ad88e2c0800e64ebbb
Peter Johnson <insomnia@rcpt.at>
2014-10-16 13:39:39 +0100

    remove redundant test

--- test/run.js
+++ test/run.js
@@ -6,7 +6,6 @@ var tests = [
   require('./util/centroidCodec'),
   require('./stream/osm/any/buildHierarchy'),
   require('./stream/node_filter'),
-  require('./stream/way_filter'),
   require('./stream/address_extractor'),
   require('./stream/stats'),
   require('./stream/osm_types'),
