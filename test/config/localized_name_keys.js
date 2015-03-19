0910ce00e45836c9003d1167ce698afc86e63d63
Peter Johnson <wiz@wiz.co.nz>
2015-03-19 17:18:47 +0100

    localized_keys -> localized_name_keys

--- test/config/localized_name_keys.js
+++ test/config/localized_name_keys.js
@@ -1,12 +1,12 @@
 
-var localized_keys = require('../../config/localized_keys');
+var localized_name_keys = require('../../config/localized_name_keys');
 
 module.exports.tests = {};
 
 // test exports
 module.exports.tests.interface = function(test, common) {
-  test('interface: localized_keys', function(t) {
-    t.true(Array.isArray(localized_keys), 'valid key list');
+  test('interface: localized_name_keys', function(t) {
+    t.true(Array.isArray(localized_name_keys), 'valid key list');
     t.end();
   });
 };
@@ -14,7 +14,7 @@ module.exports.tests.interface = function(test, common) {
 module.exports.all = function (tape, common) {
 
   function test(name, testFunction) {
-    return tape('localized_keys: ' + name, testFunction);
+    return tape('localized_name_keys: ' + name, testFunction);
   }
 
   for( var testCase in module.exports.tests ){
