6c2e4eb4e7da12f19f24f5ed3014fec3d05766b7
Julian Simioni <julian@simioni.org>
2016-02-29 23:06:55 -0500

    Remove built in document mapper stream

--- test/run.js
+++ test/run.js
@@ -8,7 +8,6 @@ var tests = [
   require('./config/localized_name_keys'),
   require('./stream/address_extractor'),
   require('./stream/document_constructor'),
-  require('./stream/dbmapper'),
   require('./stream/denormalizer'),
   require('./stream/pbf'),
   require('./stream/stats'),
