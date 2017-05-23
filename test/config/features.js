2bd7a8c46e7ef866bf1262df073d0d6b13668934
Peter Johnson <insomnia@rcpt.at>
2017-05-23 15:34:52 +0200

    features: improved tests

--- test/config/features.js
+++ test/config/features.js
@@ -11,6 +11,52 @@ module.exports.tests.interface = function(test, common) {
   });
 };
 
+// ensure some tags are excluded
+module.exports.tests.blacklist = function(test, common) {
+  test('blacklist', function(t) {
+    // see: https://github.com/pelias/openstreetmap/pull/280
+    t.true( features.indexOf('aeroway+name') <0 );
+    t.true( features.indexOf('aeroway~gate+name') <0 );
+    t.end();
+  });
+};
+
+// ensure some tags are included
+module.exports.tests.whitelist = function(test, common) {
+  test('whitelist', function(t) {
+    t.false( features.indexOf('addr:housenumber+addr:street') <0 );
+    t.false( features.indexOf('amenity+name') <0 );
+    t.false( features.indexOf('building+name') <0 );
+    t.false( features.indexOf('shop+name') <0 );
+    t.false( features.indexOf('office+name') <0 );
+    t.false( features.indexOf('public_transport+name') <0 );
+    t.false( features.indexOf('cuisine+name') <0 );
+    t.false( features.indexOf('railway+name') <0 );
+    t.false( features.indexOf('sport+name') <0 );
+    t.false( features.indexOf('natural+name') <0 );
+    t.false( features.indexOf('tourism+name') <0 );
+    t.false( features.indexOf('leisure+name') <0 );
+    t.false( features.indexOf('historic+name') <0 );
+    t.false( features.indexOf('man_made+name') <0 );
+    t.false( features.indexOf('landuse+name') <0 );
+    t.false( features.indexOf('waterway+name') <0 );
+    t.false( features.indexOf('aerialway+name') <0 );
+    t.false( features.indexOf('craft+name') <0 );
+    t.false( features.indexOf('military+name') <0 );
+    t.false( features.indexOf('aeroway~terminal+name') <0 );
+    t.false( features.indexOf('aeroway~aerodrome+name') <0 );
+    t.false( features.indexOf('aeroway~helipad+name') <0 );
+    t.false( features.indexOf('aeroway~airstrip+name') <0 );
+    t.false( features.indexOf('aeroway~heliport+name') <0 );
+    t.false( features.indexOf('aeroway~areodrome+name') <0 );
+    t.false( features.indexOf('aeroway~spaceport+name') <0 );
+    t.false( features.indexOf('aeroway~landing_strip+name') <0 );
+    t.false( features.indexOf('aeroway~airfield+name') <0 );
+    t.false( features.indexOf('aeroway~airport+name') <0 );
+    t.end();
+  });
+};
+
 module.exports.all = function (tape, common) {
 
   function test(name, testFunction) {
