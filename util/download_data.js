6f32205d8465fafc3eea1865921ee666129db1d0
Diana Shkolnikov <dianashk@gmail.com>
2017-07-10 09:40:23 -0400

    remove unneccessary `cd`

--- util/download_data.js
+++ util/download_data.js
@@ -52,7 +52,7 @@ function downloadSource(config, sourceUrl, callback) {
   const targetDir = config.imports.openstreetmap.datapath;
 
   logger.debug(`downloading ${sourceUrl} to ${targetDir}`);
-  child_process.exec(`cd ${targetDir} && { curl -L -X GET --silent --remote-name ${sourceUrl}; cd -; }`, callback);
+  child_process.exec(`cd ${targetDir} && { curl -L -X GET --silent --remote-name ${sourceUrl}; }`, callback);
 }
 
 module.exports = download;
