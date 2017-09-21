c3be78f4b0ee1de373a2db4a7f06098a59d754e8
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-09-21 12:57:22 +0300

    Use the bounding box computed by the new parser

--- stream/denormalizer.js
+++ stream/denormalizer.js
@@ -8,8 +8,6 @@
 **/
 
 var through = require('through2'),
-    isObject = require('is-object'),
-    geometryUtil = require('../util/geometryUtil'),
     peliasLogger = require( 'pelias-logger' ).get( 'openstreetmap' ),
     _ = require('lodash');
 
@@ -18,45 +16,30 @@ function denormalizer(){
   return through.obj( function( doc, enc, next ){
 
     // skip non-way docs
-    if( doc.getId().indexOf('way') === -1 ){
+    if( doc.getId().indexOf('way') === -1 && doc.getId().indexOf('relation') === -1){
       this.push( doc );
       return next();
     }
 
     try {
 
-      var nodes = doc.getMeta('nodes');
-      if( nodes ){
-
-        var points = nodes.map( function( doc ){
-          return {
-            longitude: doc.lon,
-            latitude: doc.lat
-          };
-        });
-
-        // compute the bounding box of the geometry
-        var bbox = geometryUtil.computeBBox( points );
-        if( isObject( bbox ) ) {
-          doc.setBoundingBox(bbox);
-        }
-
-        // only compute centroids where they have not already
-        // been computed (avoid doing double work)
-        if (_.isEmpty(doc.getCentroid())) {
-          var center = geometryUtil.computeCenter(points);
-          if (isObject(center)) {
-            doc.setCentroid(center);
-          }
-        }
-
+      var bboxmin = doc.getMeta('BBoxMin');
+      var bboxmax = doc.getMeta('BBoxMax');
+      if( bboxmin && bboxmax ) {
+        var bbox = {
+          min_lat: bboxmin.lat,
+          max_lat: bboxmax.lat,
+          min_lon: bboxmin.lon,
+          max_lon: bboxmax.lon
+        };
+        doc.setBoundingBox(bbox);
       }
 
       this.push( doc );
       next();
 
     } catch( e ){
-      peliasLogger.error( 'failed to denormalize way', e );
+      peliasLogger.error( 'failed to denormalize', e );
       next();
     }
   });
