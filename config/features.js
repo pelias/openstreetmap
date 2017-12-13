e58ab5e08a7e51884d48ac2ee3a2a96cfbc36420
JBGriesner <jb.griesner@qwant.com>
2017-12-13 17:32:01 +0100

    Remove POI from importer -- do NOT merge

--- config/features.js
+++ config/features.js
@@ -5,39 +5,7 @@
 **/
 
 var tags = [
-  'addr:housenumber+addr:street',
-  'amenity+name',
-  'building+name',
-  'shop+name',
-  'office+name',
-  'public_transport+name',
-  'cuisine+name',
-  'railway~station+name',
-  'railway~tram_stop+name',
-  'railway~halt+name',
-  'railway~subway_entrance+name',
-  'railway~train_station_entrance+name',
-  'sport+name',
-  'natural+name',
-  'tourism+name',
-  'leisure+name',
-  'historic+name',
-  'man_made+name',
-  'landuse+name',
-  'waterway+name',
-  'aerialway+name',
-  'craft+name',
-  'military+name',
-  'aeroway~terminal+name',
-  'aeroway~aerodrome+name',
-  'aeroway~helipad+name',
-  'aeroway~airstrip+name',
-  'aeroway~heliport+name',
-  'aeroway~areodrome+name',
-  'aeroway~spaceport+name',
-  'aeroway~landing_strip+name',
-  'aeroway~airfield+name',
-  'aeroway~airport+name'
+  'addr:housenumber+addr:street'
 ];
 
 module.exports = tags;
