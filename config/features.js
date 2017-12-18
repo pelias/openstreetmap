4559cf1989b5e39585fe83fa28187ad36873f108
JBGriesner <jb.griesner@qwant.com>
2017-12-18 17:06:08 +0100

    Remove POIs from importer depending on parameters

--- config/features.js
+++ config/features.js
@@ -4,8 +4,45 @@
  imports. @see: https://github.com/pelias/pbf2json for more info.
 **/
 
+// default tags imported
 var tags = [
   'addr:housenumber+addr:street'
 ];
 
-module.exports = tags;
+// tags corresponding to venues
+var tags_venues = [
+  'amenity+name',
+  'building+name',
+  'shop+name',
+  'office+name',
+  'public_transport+name',
+  'cuisine+name',
+  'railway~station+name',
+  'railway~tram_stop+name',
+  'railway~halt+name',
+  'railway~subway_entrance+name',
+  'railway~train_station_entrance+name',
+  'sport+name',
+  'natural+name',
+  'tourism+name',
+  'leisure+name',
+  'historic+name',
+  'man_made+name',
+  'landuse+name',
+  'waterway+name',
+  'aerialway+name',
+  'craft+name',
+  'military+name',
+  'aeroway~terminal+name',
+  'aeroway~aerodrome+name',
+  'aeroway~helipad+name',
+  'aeroway~airstrip+name',
+  'aeroway~heliport+name',
+  'aeroway~areodrome+name',
+  'aeroway~spaceport+name',
+  'aeroway~landing_strip+name',
+  'aeroway~airfield+name',
+  'aeroway~airport+name'
+];
+
+module.exports = { tags,tags_venues}
